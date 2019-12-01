50d4e60d5615d7bff53324a8416687d2cbaf7cf7
Pandu Agung Wijaya <55350608+udnapaw@users.noreply.github.com>
2019-12-01 20:47:40 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,9 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     $router->post('register', 'AuthController@register');
 
     $router->post('login', 'AuthController@login');
+
+    $router->get('profile', 'UserController@profile');
+    $router->get('users/{id}', 'UserController@singleUser');    
+    $router->get('users', 'UserController@allUsers');
  
  });
